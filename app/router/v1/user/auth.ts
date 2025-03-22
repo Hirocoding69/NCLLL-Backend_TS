e4fbf5e8763de2ed371b7b7713b335648e6d16ba
Hirohori <somborteng1@gmail.com>
2025-03-23 06:03:56 +0700

    push

--- app/router/v1/user/auth.ts
+++ app/router/v1/user/auth.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { AuthController } from "~/app/controller/v1/user/auth";
 import { bindCtx } from "~/common/utils/bind";
 import { vbody } from "~/common/middleware/validator";
-import { ChangePasswordPayload, CompareSecondaryPasswordPayload, LoginPayload, RecoverPasswordPayload, RegisterPayload } from "~/app/dto/auth";
+import { ChangePasswordPayload, LoginPayload} from "~/app/dto/auth";
 
 const router = express.Router();
 const ctrl = bindCtx(new AuthController());
