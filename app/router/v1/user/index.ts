6a3d73b0f1011f7293f7a4f4574890948d3af8bd
DamonKert <125014031+DamonKert@users.noreply.github.com>
2025-03-29 00:21:47 +0700

    Add API For submit Request Partner

--- app/router/v1/user/index.ts
+++ app/router/v1/user/index.ts
@@ -7,11 +7,14 @@ import TagRoutes from './tag';
 import GavormentRoutes from './govern-member';
 import MinistryRoutes from './ministry';
 import ResourceRoutes from './resource';
+import RequestPartnersRoutes from './request-partners';
+
 router
     .use('/govern-members', GavormentRoutes)
     .use('/ministries', MinistryRoutes)
     .use('/tags', TagRoutes)
     .use('/banners', BannerRoutes)
+    .use('/request-partner', RequestPartnersRoutes)
     .use('/resources', ResourceRoutes);
 
 export default router;
---
+++ app/service/request-partner.ts
@@ -0,0 +1,21 @@
+import { CreateRequestPartnerDTO } from '../dto/request-partner';
+import { RequestPartnersModel } from '../entity/request-partners';
+
+export class RequestPartnerService {
+  /**
+   * Create a new ministry
+   * @param payload Ministry creation data
+   * @returns Newly created ministry
+   */
+  async create(payload: CreateRequestPartnerDTO) {
+
+    return await RequestPartnersModel.create({
+      status: "pending",
+      email: payload.email,
+      reason: payload.reason,
+      description: payload.description,
+      created_at: new Date(),
+      updated_at: new Date()
+    });
+  }
+}
